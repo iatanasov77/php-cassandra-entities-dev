40115ac47fd94f719fdd3761679c765cf8fc7b8a
Ivan Atanasov (Accenture) <ivan.atanasov@accenture.com>
2019-04-04 17:42:00 +0300

    Amend commit ;)

--- Vagrant/provision/httpd_config.sh
+++ Vagrant/provision/httpd_config.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+echo "Provsioning guest with httpd config..."
 
 # Install Cassandra Cluster Admin
-git clone https://github.com/sebgiroux/Cassandra-Cluster-Admin.git /home/vagrant/www/CassandraClusterAdmin
+git clone https://github.com/iatanasov77/Cassandra-Cluster-Admin.git /home/vagrant/www/CassandraClusterAdmin
 sudo php /usr/local/bin/mkvhost -scassandra.pcedev.lh -d/home/vagrant/www/CassandraClusterAdmin -tsimple -f
-
-echo "Provsioning guest with httpd config..."
 
 sudo php /usr/local/bin/mkvhost -spcedev.lh -d/vagrant/webroot -tsimple -f
